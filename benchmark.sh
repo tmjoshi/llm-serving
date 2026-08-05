4011c375118c69a075db338614a818c0302a044b
Tejas <tmjoshi@usc.edu>
2026-08-04 22:56:47 -0700

    test 1

--- benchmark.sh
+++ benchmark.sh
@@ -2,8 +2,10 @@
 wget https://huggingface.co/datasets/anon8231489123/ShareGPT_Vicuna_unfiltered/resolve/main/ShareGPT_V3_unfiltered_cleaned_split.json
 
 vllm bench serve \
-  --model Qwen/Qwen2.5-1.5B-Instruct \
-  --endpoint /v1/completions \
-  --dataset-name sharegpt \
-  --dataset-path ShareGPT_V3_unfiltered_cleaned_split.json \
-  --num-prompts 10
+    --model Qwen/Qwen2.5-1.5B-Instruct \
+    --endpoint /v1/completions \
+    --dataset-name sharegpt \
+    --dataset-path ShareGPT_V3_unfiltered_cleaned_split.json \
+    --num-prompts 50 \
+    --num-prompts 5000 \
+    --request-rate 20
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-ssh ubuntu@147.224.38.35
+ssh ubuntu@#
 
 
