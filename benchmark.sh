7d7c84c0c571e0af0c11121f58a6f9e5d816c33a
Tejas <tmjoshi@usc.edu>
2026-08-06 12:37:36 -0700

    lambda labs instance

--- benchmark.sh
+++ benchmark.sh
@@ -7,4 +7,5 @@ vllm bench serve \
     --dataset-name sharegpt \
     --dataset-path ShareGPT_V3_unfiltered_cleaned_split.json \
     --num-prompts 5000 \
-    --request-rate 20
+    --request-rate 20 \
+    --max-concurrency 1
